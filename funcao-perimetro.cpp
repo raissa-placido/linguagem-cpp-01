31d699163166bf2603490c63f7b7834fc525ffee
raisspl <141676824+raissa-placido@users.noreply.github.com>
2025-09-18 20:00:54 +0000

    okokok

--- funcao-perimetro.cpp
+++ funcao-perimetro.cpp
@@ -1,14 +1,14 @@
 #include <iostream>
 
-int perimetro(int a, int b){
-    int r = a *2+ b*2;
+long long perimetro(int a, int b){
+    long long r = a * 2LL + b* 2LL;
     return r;
 }
 
 int main(){
     int a,b;
     std::cin >> a >> b;
-    int p = perimetro(a,b);
+    long long p = perimetro(a,b);
     std::cout << p << std::endl;
     return 0;
 }
